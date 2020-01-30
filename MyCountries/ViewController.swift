8d46eb2562856e093b22bf3644726086cedbb561
Viktoria Rohozhyna <viktoria.rohozhyna@teamvoy.com>
2020-01-30 10:41:17 +0200

    Initial Commit

---
+++ MyCountries/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyCountries
+//
+//  Created by Вікторія Рогожина on 30.01.2020.
+//  Copyright © 2020 Viktoria Rohozhyna. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
